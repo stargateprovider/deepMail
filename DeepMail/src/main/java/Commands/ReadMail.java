06c182679683a48ed30ca9bed4039e7ce42d65e4
stargateprovider <kana46@windowslive.com>
2020-03-27 23:32:56 +0200

    Täiendasin meilide lugemist ja kaustade valimist

--- DeepMail/src/main/java/Commands/ReadMail.java
+++ DeepMail/src/main/java/Commands/ReadMail.java
@@ -7,25 +7,26 @@ import picocli.CommandLine.*;
 import javax.mail.*;
 import java.awt.*;
 import java.io.*;
+import java.nio.file.Files;
 import java.util.*;
 import java.util.concurrent.Callable;
 
 /**
  * Prindib kõik meiliserveri foldername kaustas olevad emailid
- * Kasuta nii: ReadMail <meiliserver> <emaili aadress> -p [parool] [kirjade kaust]
+ * Kasuta nii: ReadMail <emaili aadress> [-s <meiliserver>]
  */
 @Command(name = "readmail", mixinStandardHelpOptions = true)
 public class ReadMail implements Callable<Integer> {
-    @Parameters(arity="2")
-    String[] args;
-    @Option(names = {"-p", "--password"}, required = true, interactive = true)
-    char[] password;
-    @Option(names = {"-f", "--folder"}, defaultValue = "inbox")
-    String folderName;
+    @Parameters(arity="1")
+    String email;
+    @Option(names = {"-s", "--server"}, description = "Incoming mail server", defaultValue = "")
+    String server;
 
     Message[] messages;
-
-    IMAPFolder folder;
+    Store store;
+    IMAPFolder currentFolder;
+    ArrayList<Folder> folders;
+    ArrayList<String> folderNames;
 
     public static void main(String[] args) {
         new CommandLine(new ReadMail()).execute(args);
@@ -33,43 +34,75 @@ public class ReadMail implements Callable<Integer> {
 
     @Override
     public Integer call() throws MessagingException {
+        if (server.isEmpty())
+            server = MailTools.identifyMailServer(email, true);
+
         Properties props = System.getProperties();
         props.setProperty("mail.store.protocol", "imaps");
+        props.setProperty("mail.imap.starttls.enable", "true");
+        //props.setProperty("mail.imap.auth.xoauth2.disable", "false");
+        props.setProperty("mail.imap.sasl.enable", "true");
 
-        Session session = Session.getDefaultInstance(props, null);
-        Store store = null;
-        folder = null;
+        Session session = Session.getInstance(props, null);
         try {
             store = session.getStore("imaps");
-            store.connect(args[0], args[1], String.valueOf(password));
-            System.out.println("Logged in as " + args[1]);
+            store.connect(server, email, String.valueOf(CommandExecutor.readPassword()));
+            System.out.println("Logged in as " + email);
 
-            folder = (IMAPFolder) store.getFolder(folderName);
-            if (!folder.isOpen())
-                folder.open(Folder.READ_WRITE);
-            messages = folder.getMessages();
+            // Kausta valimine
+            folders = new ArrayList<>();
+            folderNames = new ArrayList<>();
 
-            for (int i = messages.length; i > 0; i--) {
-                Message msg = messages[i - 1];
-                System.out.println(i + ".\tFrom: " + Arrays.toString(msg.getFrom())
-                        + "\n\tSubject: " + msg.getSubject()
-                        + "\n\tDate: " + msg.getSentDate());
+            for (Folder folder : store.getDefaultFolder().list("*")) {
+                if ((folder.getType() & Folder.HOLDS_MESSAGES) != 0) {
+                    folders.add(folder);
+                    folderNames.add(folder.getFullName() + ": " + folder.getMessageCount());
+                }
             }
+            new SelectFolder().call();
 
+            // Meilidevaade
             HashMap<String, Callable<Integer>> commands = new HashMap<>();
             commands.put("print", new ReadMsg());
             //commands.put("reply", new ReplyMsg());
             //commands.put("write", new WriteMsg());
             commands.put("delete", new DeleteMsg());
+            commands.put("folder", new SelectFolder());
 
             CommandExecutor cmdExecutor = new CommandExecutor(commands);
             cmdExecutor.run();
             System.out.println("Logged out");
-        } finally {
-            //if (folder != null && folder.isOpen()) folder.close(true);
-            //if (store != null) store.close();
+        } /* LISA TINGIMUS: KUI ON UUS GMAIL KASUTAJA>> catch (AuthenticationFailedException e){
+            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE))
+                Desktop.getDesktop().browse(URI.create("https://myaccount.google.com/lesssecureapps"));
+        }*/ finally {
+            //close();
         }
         return 0;
+    }
+
+    public void getMessages() throws MessagingException {
+        messages = currentFolder.getMessages();
+
+        for (int i = messages.length; i > 0; i--) {
+            Message msg = messages[i - 1];
+
+            String newPrefix = "";
+            if (!msg.isSet(Flags.Flag.SEEN)) {
+                newPrefix = "(NEW) ";
+            }
+
+            System.out.println(i + ".\tFrom: " + Arrays.toString(msg.getFrom())
+                    + "\n\tSubject: " + msg.getSubject()
+                    + "\n\t" + newPrefix + "Date: " + msg.getSentDate());
+        }
+    }
+
+    public void close() throws MessagingException {
+        if (currentFolder != null && currentFolder.isOpen())
+            currentFolder.close(true);
+        if (store != null)
+            store.close();
     }
 
 
@@ -78,7 +111,7 @@ public class ReadMail implements Callable<Integer> {
      */
     @Command(name = "readmsg", mixinStandardHelpOptions = true)
     class ReadMsg implements Callable<Integer> {
-        @Option(names = {"-i", "--number"}, required = true, arity = "1..*")
+        @Parameters(arity = "1")
         int msgNumber;
 
         @Override
@@ -88,11 +121,11 @@ public class ReadMail implements Callable<Integer> {
             if (messages[msgNumber-1].isMimeType("multipart/*") && Desktop.isDesktopSupported()
                     && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
 
-                File file = new File("temp.html");
-                try (FileWriter writer = new FileWriter(file)) {
+                File tempFile = Files.createTempFile("msg", ".html").toFile();
+                try (FileWriter writer = new FileWriter(tempFile)) {
                     writer.write(msgContent);
                 }
-                Desktop.getDesktop().browse(file.toURI());
+                Desktop.getDesktop().browse(tempFile.toURI());
 
             } else {
                 System.out.println(msgContent);
@@ -171,8 +204,8 @@ public class ReadMail implements Callable<Integer> {
                 if(result.toLowerCase().equals("y")) {
 
                     msg.setFlag(Flags.Flag.DELETED, true);
-                    if(folder.isOpen()){
-                        folder.expunge();
+                    if(currentFolder.isOpen()){
+                        currentFolder.expunge();
                         System.out.println("Email is deleted!");
                     }
                     else{
@@ -190,7 +223,28 @@ public class ReadMail implements Callable<Integer> {
             return 0;
         }
     }
+
+    /**
+     * Valib meilide kausta
+     * Süntaks: folder <number>
+     */
+    @Command(name = "folder", mixinStandardHelpOptions = true)
+    class SelectFolder implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws MessagingException {
+
+            int folderIndex = CommandExecutor.quickChoice(folderNames, "\n");
+            String folderName = folders.get(folderIndex).getFullName();
+
+            if (currentFolder != null && currentFolder.isOpen())
+                currentFolder.close();
+            currentFolder = (IMAPFolder) store.getFolder(folderName);
+            if (!currentFolder.isOpen())
+                currentFolder.open(Folder.READ_WRITE);
+
+            getMessages();
+            return 0;
+        }
+    }
 }
-
-
-
