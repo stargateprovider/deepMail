f2cff4bfcbed4f6910b9698581e8f309562a0f99
Ahto Türkson <ahto.tyrkson@gmail.com>
2020-04-03 13:20:01 +0300

    Algne kasutaja ja Emaili klass

--- DeepMail/src/main/java/Account.java
+++ DeepMail/src/main/java/Account.java
@@ -1,7 +1,23 @@
+import java.util.List;
+
 public class Account {
 
     private String username;
     private byte[] hashedPassword;
 
+    private List<Email> emailsList;
 
+    public Account(String username, String hashedPassword) {
+        this.username = username;
+        //Tuleb asendada hashimiseks mõeldud meetodi kutsumisega
+        this.hashedPassword = hashedPassword.getBytes();
+    }
+
+    public List<Email> getEmailsList() {
+        return emailsList;
+    }
+
+    public void addEmail(String domain, String password) {
+        this.emailsList.add(new Email(domain, password));
+    }
 }
